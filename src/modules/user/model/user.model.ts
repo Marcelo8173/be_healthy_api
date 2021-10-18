7f7273b89df116f8c07111f69e5f3cd5061967f8
Marcelo nascimento <marcelo.nascimento@skyone.solutions>
2021-10-18 19:35:14 -0300

    create new user

---
+++ src/modules/user/model/user.model.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+class UserModel {
+
+    @PrimaryGeneratedColumn('uuid')
+    id!: string; 
+
+    @Column()
+    name!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    password!: string;
+    
+    @CreateDateColumn()
+    created_at!: Date;
+
+    @UpdateDateColumn()
+    updated_at!: Date;
+}
+
+export { UserModel };
--- src/modules/user/routes/user.routes.ts
+++ src/modules/user/routes/user.routes.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
+import { getRepository } from 'typeorm';
+import { UserModel } from '../model/user.model';
+import { v4 as uuid} from 'uuid';
+
 
 const userRouter = Router();
 
-userRouter.post('/', (request,response) => {
-    const {name,email} = request.body;
-    console.log(name, email);
+userRouter.post('/', async (request,response) => {
+    const {name,email, password} = request.body;
+    // salvar um novo usuario
+    const repository = getRepository(UserModel);
+
+    const users = repository.create({
+        id: uuid(), 
+        name,
+        email, 
+        password
+    })
+    const userSaved = await repository.save(users);
+
+    return response.status(201).json(userSaved);
 })
 
 export { userRouter };
